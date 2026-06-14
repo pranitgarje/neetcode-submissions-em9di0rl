3afbd78f891ebc706908bd1592af22c54e6f66e7
Pranit Garje <145798451+pranitgarje@users.noreply.github.com>
2026-06-14 18:55:04 +0530

    Add: meeting-schedule-ii - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-2.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition of Interval:
+ * class Interval {
+ * public:
+ *     int start, end;
+ *     Interval(int start, int end) {
+ *         this->start = start;
+ *         this->end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    int minMeetingRooms(vector<Interval>& intervals) {
+        int n=intervals.size();
+        if(n<=0){
+            return 0;
+        }
+        sort(intervals.begin(),intervals.end(),[](Interval &a,Interval &b){
+            return a.start < b.start;
+        });
+        priority_queue<int,vector<int>,greater<int>> pq;
+       
+        pq.push(intervals[0].end);
+        for(int i=1;i<n;i++){
+            if (intervals[i].start >= pq.top()){
+                pq.pop();
+            }
+            pq.push(intervals[i].end);
+        }
+
+        return pq.size();
+        
+    }
+};
