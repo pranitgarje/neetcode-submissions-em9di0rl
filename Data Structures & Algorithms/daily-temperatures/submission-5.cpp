582f8fcea9df7c067adcc0a25430406fa8f36b9e
Pranit Garje <145798451+pranitgarje@users.noreply.github.com>
2026-07-19 23:13:39 +0530

    Add: daily-temperatures - submission-5

---
+++ Data Structures & Algorithms/daily-temperatures/submission-5.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        // next greater temperature i.e NGE-> decresing stack.
+        int n=temperatures.size();
+        stack<int> st;
+        vector<int> ans(n,0);
+        for(int i=0;i<n;i++){
+            while(!st.empty() && temperatures[st.top()]<temperatures[i]){
+                int idx=st.top();
+                st.pop();
+                ans[idx]=i-idx;
+            }
+            st.push(i) ;
+        }
+        return ans;
+        
+    }
+};
