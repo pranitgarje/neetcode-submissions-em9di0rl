e27dfab027931f31f56509f30fed5d4c4038c017
Pranit Garje <145798451+pranitgarje@users.noreply.github.com>
2026-07-25 19:14:59 +0530

    Add: course-schedule - submission-0

---
+++ Data Structures & Algorithms/course-schedule/submission-0.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+            int v=numCourses;
+            vector<vector<int>> adj(v);
+            vector<int> indegree(v,0);
+            for(auto &edge:prerequisites){
+                int u=edge[1];
+                int v=edge[0];
+                adj[u].push_back(v);
+                indegree[v]++ ;
+            }
+            queue<int> q;
+            for(int i=0;i<v;i++){
+                if(indegree[i]==0){
+                    q.push(i);
+                }
+            }
+            vector<int> topo;
+            while(!q.empty()){
+                int curr=q.front();
+                q.pop();
+                topo.push_back(curr);
+                for(int ngb:adj[curr]){
+                    indegree[ngb]-- ;
+                    if(indegree[ngb]==0) q.push(ngb);
+                }
+            }
+            if(topo.size()==v) return true;
+            return false;
+    }
+};
